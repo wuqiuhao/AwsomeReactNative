74cea79185864c7da2fab91bd96ee409a983e07c
wuqiuhao <wqiuhao@gmail.com>
2018-04-15 15:55:44 +0800

    init project

---
+++ AwsomeReactNative/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwsomeReactNative
+//
+//  Created by Hale on 2018/4/15.
+//  Copyright © 2018年 Hale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
